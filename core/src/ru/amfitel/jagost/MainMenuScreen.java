a0a594673c6ae42b4ff04e01e8d3ae0c2c3932b7
JagoST7 <JagoST7@gmail.com>
2016-12-11 17:30:59 +0300

    EntryPoint

---
+++ core/src/ru/amfitel/jagost/MainMenuScreen.java
@@ -0,0 +1,77 @@
+package ru.amfitel.jagost;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by st_ni on 11.12.2016.
+ */
+public class MainMenuScreen implements Screen {
+
+    OrthographicCamera camera;
+    SpriteBatch batch;
+    BitmapFont font;
+
+    public MainMenuScreen(final Game gam) {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        font.draw(batch, "Welcome to Drop!!! ", 100, 150);
+        font.draw(batch, "Tap anywhere to begin!", 100, 100);
+        batch.end();
+
+        if (Gdx.input.isTouched()) {
+//            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
