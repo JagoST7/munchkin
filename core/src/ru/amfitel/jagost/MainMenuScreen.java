a5ad3032b172be66746fc317aa3707ba0db831ce
JagoST7 <Zero782>
2016-12-14 12:46:33 +0300

    basicSkin

--- core/src/ru/amfitel/jagost/MainMenuScreen.java
+++ core/src/ru/amfitel/jagost/MainMenuScreen.java
@@ -4,10 +4,10 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import ru.amfitel.jagost.menu.BasicSkin;
 
 /**
  * Created by st_ni on 11.12.2016.
@@ -27,7 +27,7 @@ public class MainMenuScreen implements Screen {
 
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);// Make the stage consume events
-        createBasicSkin();
+        skin = BasicSkin.getSkin();
 
          newGameButton = new TextButton("New game", skin); // Use the initialized skin
         newGameButton.setPosition(Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8, Gdx.graphics.getHeight() / 2);
@@ -88,25 +88,4 @@ public class MainMenuScreen implements Screen {
         stage.dispose();
     }
 
-    private void createBasicSkin() {
-        //Create a font
-        BitmapFont font = new BitmapFont();
-        skin = new Skin();
-        skin.add("default", font);
-
-        //Create a texture
-        Pixmap pixmap = new Pixmap((int) Gdx.graphics.getWidth() / 4, (int) Gdx.graphics.getHeight() / 10, Pixmap.Format.RGB888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        skin.add("background", new Texture(pixmap));
-
-        //Create a button style
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
-        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
-        textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
-        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
-        textButtonStyle.font = skin.getFont("default");
-        skin.add("default", textButtonStyle);
-    }
 }
